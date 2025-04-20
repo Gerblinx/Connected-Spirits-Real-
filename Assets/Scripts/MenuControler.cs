41ec9c718e5f956e69d472a9104226a990d2420f
Shay <shayberry2015@gmail.com>
2025-04-20 00:03:37 -0400

    Menu part2

--- Assets/Scripts/MenuControler.cs
+++ Assets/Scripts/MenuControler.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MenuControler : MonoBehaviour
 {
+    public GameObject menuCanvas;
+    InputAction controlsAction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controlsAction = InputSystem.actions.FindAction("Menu");
+
+        menuCanvas.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (controlsAction.WasPressedThisFrame())
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+
+        }
     }
 }
