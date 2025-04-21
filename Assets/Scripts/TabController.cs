adfdad113d5f84047b4999885c83dd3a8b39ae4d
Shay <shayberry2015@gmail.com>
2025-04-20 21:15:47 -0400

    button click

--- Assets/Scripts/TabController.cs
+++ Assets/Scripts/TabController.cs
@@ -2,29 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class TabController : MonoBehaviour
 {
 
     public Image[] tabImages;
     public GameObject[] pages;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         ActivateTab(0);
+       
     }
 
-    // Update is called once per frame
     public void ActivateTab(int tabNO)
     {
         for(int i = 0; i < pages.Length; i++)
         {
             pages[i].SetActive(false);
             tabImages[i].color = Color.grey;
+            
         }
+
         pages[tabNO].SetActive(true);
         tabImages[tabNO].color = Color.white;
+        
+       
 
     }
 }
