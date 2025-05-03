ea6f2c0a31744779b05014897def0053be192c8c
Shay <shayberry2015@gmail.com>
2025-05-03 01:22:19 -0400

    Restarting >:(

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,33 +1,19 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class Movement : MonoBehaviour, IDataPersistence
+public class Movement : MonoBehaviour
 {
     public float moveSpeed = 3f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
 
-    private bool positionLoaded = false;
+   
 
     private void Awake()
     {
       
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    public void LoadData(GameData data)
-    {
-        Debug.Log("LoadData called with position: " + data.playerPosition);
-        rb.position = data.playerPosition;
-        positionLoaded = true;
-    }
-
-  
-    public void SaveData(ref GameData data)
-    {
-        data.playerPosition = rb.position;
-        Debug.Log("💾 Saving player position: " + rb.position);
     }
 
    
@@ -44,9 +30,7 @@ public class Movement : MonoBehaviour, IDataPersistence
 
     private void FixedUpdate()
     {
-        if (!positionLoaded) return;
-
         // Move the player using Rigidbody2D
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+       rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
